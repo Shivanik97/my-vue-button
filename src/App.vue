08e87edde782f12fae1e1779cf679341f579a4ef
Shivani K <shivanik.bbi18@rvce.edu.in>
2023-09-01 12:33:08 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { VButton } from './components/VButton.vue';
+</script>
+
+<template>
+  <VButton class="
+      py-2
+      px-4
+      font-semibold
+      rounded-lg
+      shadow-md
+      text-white
+      bg-green-500
+      hover:bg-green-700
+    ">My Button</VButton>
+</template>
---
+++ src/components/VButton.vue
@@ -0,0 +1,8 @@
+<template>
+    <button v-bind="$attrs">
+        <slot />
+    </button>
+</template>
+<script>
+export { default as VButton } from './VButton.vue';
+</script>
